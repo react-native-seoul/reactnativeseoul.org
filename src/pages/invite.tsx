994cef62a985d57b1f53335b3e1c486122e4874d
Hyeseong Kim <cometkim.kr@gmail.com>
2018-02-28 16:47:51 +0900

    Add a page to redirect user to slack invitation

---
+++ src/pages/invite.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Helmet from 'react-helmet'
+
+// tslint:disable-next-line:max-line-length
+const SLACK_INVITE_LINK = 'https://join.slack.com/t/reactnativeseoul/shared_invite/enQtMjY4ODI2OTgzNzE0LWQ1ZDA4Mjk3ZjNmNjZjNjYyMzEwMDVhNGJjNDJlMGVhYmI1Y2M5Y2JmMDQ5MTMwMTFiNTUyOTcxZmQwNGRjMmE'
+
+export default () => (
+    <Helmet>
+        <script>{window.location.href = SLACK_INVITE_LINK}</script>
+    </Helmet>
+)
